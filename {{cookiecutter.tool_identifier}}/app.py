70776a22ed6bbc821214c9ae3c4a4c1fb0141127
Lucas Werkmeister <mail@lucaswerkmeister.de>
2018-10-28 13:00:20 +0100

    Add simple greeting page
    This demonstrates route variables and template variables.

--- {{cookiecutter.tool_identifier}}/app.py
+++ {{cookiecutter.tool_identifier}}/app.py
@@ -9,3 +9,8 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
+@app.route('/greet/<name>')
+def greet(name):
+    return flask.render_template('greet.html',
+                                 name=name)
