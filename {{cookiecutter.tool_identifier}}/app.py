c0755a93f5e3599fbbd9707bf42aa05ca32fb588
Lucas Werkmeister <mail@lucaswerkmeister.de>
2018-10-29 13:53:35 +0100

    Add OAuth authentication
    This adds an authentication area with a login link or the currently
    logged in username to the end of the navbar. The information isn’t used
    otherwise yet, but that will come later.
    
    If you access the OAuth endpoints and OAuth is not configured, the
    request errors out, but that doesn’t affect subsequent requests (Flask
    encapsulates them sufficiently), so while that might be fixed in the
    future, I don’t see it as a priority.

--- {{cookiecutter.tool_identifier}}/app.py
+++ {{cookiecutter.tool_identifier}}/app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import flask
+import mwoauth
 import os
 import random
 import requests
@@ -23,6 +24,9 @@ try:
 except FileNotFoundError:
     print('config.yaml file not found, assuming local development setup')
     app.secret_key = 'fake'
+
+if 'oauth' in app.config:
+    consumer_token = mwoauth.ConsumerToken(app.config['oauth']['consumer_key'], app.config['oauth']['consumer_secret'])
 
 
 @app.template_global()
@@ -59,6 +63,25 @@ def user_link(user_name):
             flask.Markup(r'</bdi>') +
             flask.Markup(r'</a>'))
 
+@app.template_global()
+def authentication_area():
+    if 'oauth' not in app.config:
+        return flask.Markup()
+
+    if 'oauth_access_token' not in flask.session:
+        return (flask.Markup(r'<a id="login" class="navbar-text" href="') +
+                flask.Markup.escape(flask.url_for('login')) +
+                flask.Markup(r'">Log in</a>'))
+
+    access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
+    identity = mwoauth.identify('https://www.wikidata.org/w/index.php',
+                                consumer_token,
+                                access_token)
+
+    return (flask.Markup(r'<span class="navbar-text">Logged in as ') +
+            user_link(identity['username']) +
+            flask.Markup(r'</span>'))
+
 
 @app.route('/')
 def index():
@@ -87,3 +110,16 @@ def praise():
                                  name=name,
                                  praise=praise,
                                  csrf_error=csrf_error)
+
+@app.route('/login')
+def login():
+    redirect, request_token = mwoauth.initiate('https://www.wikidata.org/w/index.php', consumer_token, user_agent=user_agent)
+    flask.session['oauth_request_token'] = dict(zip(request_token._fields, request_token))
+    return flask.redirect(redirect)
+
+@app.route('/oauth/callback')
+def oauth_callback():
+    request_token = mwoauth.RequestToken(**flask.session['oauth_request_token'])
+    access_token = mwoauth.complete('https://www.wikidata.org/w/index.php', consumer_token, request_token, flask.request.query_string, user_agent=user_agent)
+    flask.session['oauth_access_token'] = dict(zip(access_token._fields, access_token))
+    return flask.redirect(flask.url_for('index'))
