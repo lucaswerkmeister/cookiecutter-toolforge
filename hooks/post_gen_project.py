391509e18088aabb4fce6606627b7839f6edd19a
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-04-27 20:45:05 +0200

    Add option to set up Travis CI
    This configures Travis to run the automated checks on Python 3.4
    (current default version on Toolforge) and Python 3.5 (hopefully
    available soon). If all the automated checks were disabled but Travis CI
    was still enabled, we emit a warning but don’t abort the project
    generation, in case the user wants to manually set up a Makefile (or
    configure a different script in .travis.yml).

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 if not {{ cookiecutter.set_up_pytest }}:
@@ -8,3 +9,18 @@ if not {{ cookiecutter.set_up_flake8 }} and \
    not {{ cookiecutter.set_up_mypy }} and \
    not {{ cookiecutter.set_up_pytest }}:
     os.remove('Makefile')
+
+    if not {{ cookiecutter.set_up_travis_ci }}:
+        os.remove('.travis.yml')
+    else:
+        print('\033[1m', file=sys.stderr)  # ANSI SGR 1: bold text
+        print('''
+
+You specified that Travis CI support should be enabled, but that
+neither Flake8 nor mypy nor pytest should be set up. Since this leaves
+us with no automatic checks, no Makefile has been created, and the
+`make check` command configured in .travis.yml will fail unless you
+create a custom Makefile with a `check` goal.
+
+    '''.strip(), file=sys.stderr)
+        print('\033[0m', file=sys.stderr)  # ANSI SGR 0: reset/normal
