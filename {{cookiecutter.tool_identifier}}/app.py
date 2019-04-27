c31b66bab66eb458e4034f89623391b920ce98c4
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-04-27 14:25:01 +0200

    Make hooks and generated projects flake8 compliant

--- {{cookiecutter.tool_identifier}}/app.py
+++ {{cookiecutter.tool_identifier}}/app.py
@@ -13,7 +13,9 @@ import yaml
 
 app = flask.Flask(__name__)
 
-user_agent = toolforge.set_user_agent('{{ cookiecutter.tool_identifier }}', email='{{ cookiecutter.user_email }}')
+user_agent = toolforge.set_user_agent(
+    '{{ cookiecutter.tool_identifier }}',
+    email='{{ cookiecutter.user_email }}')
 
 __dir__ = os.path.dirname(__file__)
 try:
@@ -21,17 +23,25 @@ try:
         app.config.update(yaml.safe_load(config_file))
 except FileNotFoundError:
     print('config.yaml file not found, assuming local development setup')
-    app.secret_key = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(64))
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(64))
+    app.secret_key = random_string
 
 if 'oauth' in app.config:
-    consumer_token = mwoauth.ConsumerToken(app.config['oauth']['consumer_key'], app.config['oauth']['consumer_secret'])
+    oauth_config = app.config['oauth']
+    consumer_token = mwoauth.ConsumerToken(oauth_config['consumer_key'],
+                                           oauth_config['consumer_secret'])
+    index_php = 'https://{{ cookiecutter.wiki_domain }}/w/index.php'
 
 
 @app.template_global()
 def csrf_token():
     if 'csrf_token' not in flask.session:
-        flask.session['csrf_token'] = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(64))
+        characters = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(characters) for _ in range(64))
+        flask.session['csrf_token'] = random_string
     return flask.session['csrf_token']
+
 
 @app.template_global()
 def form_value(name):
@@ -42,6 +52,7 @@ def form_value(name):
     else:
         return flask.Markup()
 
+
 @app.template_global()
 def form_attributes(name):
     return (flask.Markup(r' id="') +
@@ -51,15 +62,18 @@ def form_attributes(name):
             flask.Markup(r'" ') +
             form_value(name))
 
+
 @app.template_filter()
 def user_link(user_name):
-    return (flask.Markup(r'<a href="https://{{ cookiecutter.wiki_domain }}/wiki/User:') +
+    user_href = 'https://{{ cookiecutter.wiki_domain }}/wiki/User:'
+    return (flask.Markup(r'<a href="' + user_href) +
             flask.Markup.escape(user_name.replace(' ', '_')) +
             flask.Markup(r'">') +
             flask.Markup(r'<bdi>') +
             flask.Markup.escape(user_name) +
             flask.Markup(r'</bdi>') +
             flask.Markup(r'</a>'))
+
 
 @app.template_global()
 def authentication_area():
@@ -72,7 +86,7 @@ def authentication_area():
                 flask.Markup(r'">Log in</a>'))
 
     access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
-    identity = mwoauth.identify('https://{{ cookiecutter.wiki_domain }}/w/index.php',
+    identity = mwoauth.identify(index_php,
                                 consumer_token,
                                 access_token)
 
@@ -80,38 +94,49 @@ def authentication_area():
             user_link(identity['username']) +
             flask.Markup(r'</span>'))
 
+
 def authenticated_session():
-    if 'oauth_access_token' in flask.session:
-        access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
-        auth = requests_oauthlib.OAuth1(client_key=consumer_token.key, client_secret=consumer_token.secret,
-                                        resource_owner_key=access_token.key, resource_owner_secret=access_token.secret)
-        return mwapi.Session(host='https://{{ cookiecutter.wiki_domain }}', auth=auth, user_agent=user_agent)
-    else:
+    if 'oauth_access_token' not in flask.session:
         return None
+
+    access_token = mwoauth.AccessToken(
+        **flask.session['oauth_access_token'])
+    auth = requests_oauthlib.OAuth1(client_key=consumer_token.key,
+                                    client_secret=consumer_token.secret,
+                                    resource_owner_key=access_token.key,
+                                    resource_owner_secret=access_token.secret)
+    return mwapi.Session(host='https://{{ cookiecutter.wiki_domain }}',
+                         auth=auth,
+                         user_agent=user_agent)
 
 
 @app.route('/')
 def index():
     return flask.render_template('index.html')
 
+
 @app.route('/greet/<name>')
 def greet(name):
     return flask.render_template('greet.html',
                                  name=name)
+
 
 @app.route('/praise', methods=['GET', 'POST'])
 def praise():
     csrf_error = False
     if flask.request.method == 'POST':
         if submitted_request_valid():
-            flask.session['praise'] = flask.request.form.get('praise', 'praise missing')
+            praise = flask.request.form.get('praise', 'praise missing')
+            flask.session['praise'] = praise
         else:
             csrf_error = True
             flask.g.repeat_form = True
 
     session = authenticated_session()
     if session:
-        userinfo = session.get(action='query', meta='userinfo', uiprop='options')['query']['userinfo']
+        userinfo = session.get(action='query',
+                               meta='userinfo',
+                               uiprop='options')['query']['userinfo']
         name = userinfo['name']
         gender = userinfo['options']['gender']
         if gender == 'male':
@@ -131,23 +156,35 @@ def praise():
                                  praise=praise,
                                  csrf_error=csrf_error)
 
+
 @app.route('/login')
 def login():
-    redirect, request_token = mwoauth.initiate('https://{{ cookiecutter.wiki_domain }}/w/index.php', consumer_token, user_agent=user_agent)
-    flask.session['oauth_request_token'] = dict(zip(request_token._fields, request_token))
+    redirect, request_token = mwoauth.initiate(index_php,
+                                               consumer_token,
+                                               user_agent=user_agent)
+    flask.session['oauth_request_token'] = dict(zip(request_token._fields,
+                                                    request_token))
     return flask.redirect(redirect)
+
 
 @app.route('/oauth/callback')
 def oauth_callback():
-    request_token = mwoauth.RequestToken(**flask.session.pop('oauth_request_token'))
-    access_token = mwoauth.complete('https://{{ cookiecutter.wiki_domain }}/w/index.php', consumer_token, request_token, flask.request.query_string, user_agent=user_agent)
-    flask.session['oauth_access_token'] = dict(zip(access_token._fields, access_token))
+    request_token = mwoauth.RequestToken(
+        **flask.session.pop('oauth_request_token'))
+    access_token = mwoauth.complete(index_php,
+                                    consumer_token,
+                                    request_token,
+                                    flask.request.query_string,
+                                    user_agent=user_agent)
+    flask.session['oauth_access_token'] = dict(zip(access_token._fields,
+                                                   access_token))
     return flask.redirect(flask.url_for('index'))
 
 
 def full_url(endpoint, **kwargs):
     scheme = flask.request.headers.get('X-Forwarded-Proto', 'http')
     return flask.url_for(endpoint, _external=True, _scheme=scheme, **kwargs)
+
 
 def submitted_request_valid():
     """Check whether a submitted POST request is valid.
@@ -177,6 +214,7 @@ def submitted_request_valid():
         # from other Toolforge tools
         return False
     return True
+
 
 @app.after_request
 def deny_frame(response):
