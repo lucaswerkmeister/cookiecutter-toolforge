584fc2780aa0f98cf16dfb825c6382a853bcaf26
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-04-27 19:59:35 +0200

    Add Makefile and option to set up flake8
    The Makefile is used to run flake8, mypy, and/or pytest checks if they
    are enabled. If none of them are, the Makefile is removed.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,3 +3,8 @@ import os
 
 if not {{ cookiecutter.set_up_pytest }}:
     os.remove('test_app.py')
+
+if not {{ cookiecutter.set_up_flake8 }} and \
+   not {{ cookiecutter.set_up_mypy }} and \
+   not {{ cookiecutter.set_up_pytest }}:
+    os.remove('Makefile')
