e2f7b432a64ba9d2ab53f8947011742fe9adc242
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-01-07 23:06:04 +0100

    Replace Travis CI with GitHub actions
    Not tested yet, but I’ll try it out later this week.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,17 +14,19 @@ if not {{ cookiecutter.set_up_flake8 }} and \
    not {{ cookiecutter.set_up_pytest }}:
     os.remove('Makefile')
 
-    if not {{ cookiecutter.set_up_travis_ci }}:
-        os.remove('.travis.yml')
+    if not {{ cookiecutter.set_up_github_ci }}:
+        os.remove('.github/workflows/test.yaml')
+        os.rmdir('.github/workflows')
+        os.rmdir('.github')
     else:
         print('\033[1m', file=sys.stderr)  # ANSI SGR 1: bold text
         print('''
 
-You specified that Travis CI support should be enabled, but that
+You specified that GitHub CI support should be enabled, but that
 neither Flake8 nor mypy nor pytest should be set up. Since this leaves
 us with no automatic checks, no Makefile has been created, and the
-`make check` command configured in .travis.yml will fail unless you
-create a custom Makefile with a `check` goal.
+`make check` command configured in .github/workflows/test.yaml will
+fail unless you create a custom Makefile with a `check` goal.
 
     '''.strip(), file=sys.stderr)
         print('\033[0m', file=sys.stderr)  # ANSI SGR 0: reset/normal
