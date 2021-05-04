64c74136942d79286e2b37b340bb1e303c61345e
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-05-04 20:16:43 +0200

    Allow continuing with invalid tool name
    Generating a tool with an invalid name can be useful for several
    reasons: to rewrite an existing tool, for throwaway testing, or in case
    of Striker hiccups. Prompt the user instead of exiting unconditionally.
    (However, aborting remains the default.)
    
    Fixes #1.
    
    Also, print only a short summary of the message in bold, not the whole
    thing. That seems nicer to me.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -15,6 +15,8 @@ try:
 except HTTPError:
     # Striker returns HTTP 406 Not Acceptable for invalid tool names
     print('\033[1m', file=sys.stderr)  # ANSI SGR 1: bold text
+    print('Invalid tool name', file=sys.stderr)
+    print('\033[0m', file=sys.stderr)  # ANSI SGR 0: reset/normal
     print('''
 
 The toolsadmin API indicates that "{{ cookiecutter.tool_identifier }}"
@@ -24,8 +26,15 @@ names must be valid MediaWiki user names, and they must not be blocked
 titles on Wikitech; as of November 2018, this means that they cannot
 include the name of a Wikimedia project, such as "wikipedia-something"
 or "wikidata-other", due to the global title blacklist (see T190707).
-Please choose a different name.
 
 '''.strip(), file=sys.stderr)
+    print('\033[1m', file=sys.stderr)  # ANSI SGR 1: bold text
+    print('You should probably choose a different name.', file=sys.stderr)
     print('\033[0m', file=sys.stderr)  # ANSI SGR 0: reset/normal
-    sys.exit(1)
+    try:
+        reply = input('Continue anyways? [y/N] ')
+    except EOFError:
+        print()  # line break after prompt
+        reply = ''
+    if not reply.lower().startswith('y'):
+        sys.exit(1)
